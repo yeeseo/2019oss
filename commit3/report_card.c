c89929df50b474d056435703854336898fb7eb0c
yessir skkusosc <--unset-all>
2019-10-08 02:52:24 -0700

    report card: Print grades of each subject

--- commit3/report_card.c
+++ commit3/report_card.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-	printf("This program print report card.\n");
-	return 0;
+        int kor, eng, math;
+
+        math = 80;
+        eng = 100;
+        kor = 90;
+
+        printf("This program print report card.\n");
+
+        printf("Korean : %d\n", kor);
+        printf("English : %d\n", eng);
+        printf("Math : %d\n", math);
+        return 0;
 }
+
